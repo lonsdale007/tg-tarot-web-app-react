c00fa36fda800a2239d205c1098dabd939a0b6a7
Danila Artemov <danila.artemow2012@gmail.ru>
2024-11-23 23:54:58 +0300

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+
+  const {tg, onToggleButton} = useTelegram();
 
   useEffect(() => {
     tg.ready();
@@ -9,8 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      Таро
-      <button onClick={onClose}>Закрыть</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
